fa6981544280530c0e7b08e1a2d6eaa0425fc86d
Thomas Charbonneau <thomas.charbonneau16@gmail.com>
2022-06-27 12:55:42 -0400

    E2

--- src/Client.java
+++ src/Client.java
@@ -28,10 +28,35 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-import java.io.*;
 
-public class QuoteServer {
+import java.io.*;
+import java.net.*;
+
+public class Client {
     public static void main(String[] args) throws IOException {
-        new QuoteServerThread().start();
+
+        if (args.length != 1) {
+            System.out.println("Usage: java QuoteClient <hostname>");
+            return;
+        }
+
+        // get a datagram socket
+        DatagramSocket socket = new DatagramSocket();
+
+        // send request
+        byte[] buf = new byte[256];
+        InetAddress address = InetAddress.getByName(args[0]);
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
+        socket.send(packet);
+
+        // get response
+        packet = new DatagramPacket(buf, buf.length);
+        socket.receive(packet);
+
+        // display response
+        String received = new String(packet.getData(), 0, packet.getLength());
+        System.out.println("Quote of the Moment: " + received);
+
+        socket.close();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -29,35 +29,12 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+
+
 import java.io.*;
-import java.net.*;
-import java.util.*;
 
-public class QuoteClient {
+public class Server {
     public static void main(String[] args) throws IOException {
-
-        if (args.length != 1) {
-            System.out.println("Usage: java QuoteClient <hostname>");
-            return;
-        }
-
-        // get a datagram socket
-        DatagramSocket socket = new DatagramSocket();
-
-        // send request
-        byte[] buf = new byte[256];
-        InetAddress address = InetAddress.getByName(args[0]);
-        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
-        socket.send(packet);
-
-        // get response
-        packet = new DatagramPacket(buf, buf.length);
-        socket.receive(packet);
-
-        // display response
-        String received = new String(packet.getData(), 0, packet.getLength());
-        System.out.println("Quote of the Moment: " + received);
-
-        socket.close();
+        new QuoteServerThread().start();
     }
 }
