bca07a86b9a10055f57018a9dd5a2d903873cc53
Raphthehappy <lher0801@usherbrooke.ca>
2022-06-27 21:01:19 -0400

    recevoir et envoyer pour couche

--- src/Couche.java
+++ src/Couche.java
@@ -10,4 +10,16 @@ public abstract class Couche {
     public void setDownCouche(Couche downCouche) {
         this.downCouche = downCouche;
     }
+
+    protected abstract void recevoirUp(byte[] PDU);
+
+    protected void envoyerUp(byte[] PDU) throws TransmissionErrorException {
+        upCouche.recevoirDown(PDU);
+    }
+
+    protected abstract void recevoirDown(byte[] PDU) throws TransmissionErrorException;
+
+    protected void envoyerDown(byte[] PDU) {
+        downCouche.recevoirUp(PDU);
+    }
 }
